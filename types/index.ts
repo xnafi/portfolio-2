7626586c34735a6a9c073426068a8a46185d9ba4
xnafi <forhadkhan2011@gmail.com>
2026-06-23 16:38:47 +0600

    Add types: variants and project interface

---
+++ types/index.ts
@@ -0,0 +1,27 @@
+export type Next_Page_Url = string;
+
+export type Variant =
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'warning'
+    | 'danger'
+    | 'info'
+    | 'light'
+    | 'dark'
+    | 'link'
+    | 'no-color';
+
+export interface IProject {
+    title: string;
+    year: number;
+    description: string;
+    role: string;
+    techStack: string[];
+    thumbnail: string;
+    longThumbnail: string;
+    images: string[];
+    slug: string;
+    liveUrl?: string;
+    sourceCode?: string;
+}
