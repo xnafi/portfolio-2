959087ae94f65129902a6da4668b47d69671b74a
xnafi <forhadkhan2011@gmail.com>
2026-06-24 16:36:55 +0600

    Add SectionTitle component for headings

---
+++ src/components/Re-Ui/SectionTitle.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+import {  GiFlowerEmblem } from "react-icons/gi";
+
+interface Props {
+  icon?: ReactNode;
+  className?: string;
+  classNames?: {
+    container?: string;
+    title?: string;
+    icon?: string;
+  };
+  title: string;
+}
+
+const SectionTitle = ({ icon, title, className, classNames }: Props) => {
+  return (
+    <div
+      className={`flex items-center gap-4 mb-10 ${
+        className ?? ""
+      } ${classNames?.container ?? ""}`}
+    >
+      {icon ? (
+        icon
+      ) : (
+        <GiFlowerEmblem
+          size={40}
+          className={`animate-spin [animation-duration:15s] ${classNames?.icon ?? ""}`}
+        />
+      )}
+
+      <h2
+        className={`text-xl uppercase leading-none ${classNames?.title ?? ""}`}
+      >
+        {title}
+      </h2>
+    </div>
+  );
+};
+
+export default SectionTitle;
