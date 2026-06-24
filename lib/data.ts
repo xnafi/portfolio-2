46e7eaab727e04556b34bbb16b145aea5dee6627
xnafi <forhadkhan2011@gmail.com>
2026-06-24 14:50:56 +0600

    feat: add react-icons dependency for UI icons

---
+++ lib/data.ts
@@ -0,0 +1,80 @@
+
+import {
+  FaReact,
+  FaNodeJs,
+  FaBootstrap,
+  FaHtml5,
+  FaCss3Alt,
+  FaStripe,
+  FaMobileAlt,
+} from "react-icons/fa";
+
+import {
+  SiJavascript,
+  SiTypescript,
+  SiTailwindcss,
+  SiSass,
+  SiRedux,
+  SiExpress,
+  SiMongodb,
+  SiFirebase,
+  SiGithub,
+  SiVercel,
+  SiNetlify,
+  SiEslint,
+  SiPrettier,
+  SiAstro,
+  SiPrismic,
+  SiPwa,
+  SiMui,
+  SiNextdotjs,
+  SiGreensock,
+} from "react-icons/si";
+
+import { MdAnimation } from "react-icons/md";
+import { GiSpeedometer } from "react-icons/gi";
+import { HiOutlineSquares2X2 } from "react-icons/hi2";
+
+export const MY_STACK = [
+  { name: "React.js", icon: FaReact },
+  { name: "Next.js", icon: SiNextdotjs },
+  { name: "TypeScript", icon: SiTypescript },
+  { name: "JavaScript (ES6+)", icon: SiJavascript },
+
+  { name: "Tailwind CSS", icon: SiTailwindcss },
+  { name: "SCSS / Sass", icon: SiSass },
+  { name: "Bootstrap", icon: FaBootstrap },
+  { name: "Responsive UI", icon: FaMobileAlt },
+
+  { name: "Shadcn UI", icon: HiOutlineSquares2X2 },
+  { name: "Material UI (MUI)", icon: SiMui },
+
+  { name: "Redux Toolkit", icon: SiRedux },
+
+  { name: "Framer Motion", icon: MdAnimation },
+  { name: "GSAP", icon: SiGreensock },
+  { name: "AOS", icon: MdAnimation },
+
+  { name: "Node.js", icon: FaNodeJs },
+  { name: "Express.js", icon: SiExpress },
+  { name: "MongoDB", icon: SiMongodb },
+  { name: "Firebase", icon: SiFirebase },
+  { name: "Stripe", icon: FaStripe },
+
+  { name: "Performance Optimization", icon: GiSpeedometer },
+  { name: "Next.js SEO", icon: GiSpeedometer },
+  { name: "PWA", icon: SiPwa },
+
+  { name: "Prismic CMS", icon: SiPrismic },
+  { name: "Astro.js", icon: SiAstro },
+
+  { name: "Git & GitHub", icon: SiGithub },
+  { name: "Vercel", icon: SiVercel },
+  { name: "Netlify", icon: SiNetlify },
+  { name: "ESLint", icon: SiEslint },
+  { name: "Prettier", icon: SiPrettier },
+
+  { name: "HTML5", icon: FaHtml5 },
+  { name: "CSS3", icon: FaCss3Alt },
+];
+
