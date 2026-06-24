fbadc5b20f3af4e4a3365238eb7deef1a7937644
xnafi <forhadkhan2011@gmail.com>
2026-06-24 16:46:39 +0600

    Add Skills component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "@/src/components/About";
 import Banner from "@/src/components/Banner";
+import Skills from "@/src/components/Skills";
 
 export default function Home() {
   return (
     <div className="page-">
       <Banner />
       <AboutMe />
+      <Skills />
     </div>
   );
 }
