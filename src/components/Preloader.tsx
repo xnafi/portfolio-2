d4b6f7c10af3445f92fe02b5b8cd017d459db8ff
xnafi <forhadkhan2011@gmail.com>
2026-06-24 17:06:43 +0600

    Refactor preloader animation and cleanup code

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -1,9 +1,8 @@
 "use client";
+
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React, { useRef } from "react";
-
-gsap.registerPlugin(useGSAP);
 
 const Preloader = () => {
   const preloaderRef = useRef<HTMLDivElement>(null);
@@ -16,7 +15,7 @@ const Preloader = () => {
         },
       });
 
-      tl.to(".name-text span", {
+      tl.to(".name-letter", {
         y: 0,
         stagger: 0.05,
         duration: 0.2,
@@ -24,45 +23,53 @@ const Preloader = () => {
 
       tl.to(".preloader-item", {
         delay: 1,
-        y: "100%",
+        yPercent: 100,
         duration: 0.5,
         stagger: 0.1,
       })
-        .to(".name-text span", { autoAlpha: 0 }, "<0.5")
+        .to(
+          ".name-letter",
+          {
+            autoAlpha: 0,
+            stagger: 0.02,
+          },
+          "<0.5",
+        )
         .to(
           preloaderRef.current,
           {
             autoAlpha: 0,
+            pointerEvents: "none",
           },
           "<1",
         );
+
+      return () => tl.kill();
     },
     { scope: preloaderRef },
   );
 
-  return (
-    <div
-      className="fixed inset-0 z-6 flex max-w-350 mx-auto"
-      ref={preloaderRef}
-    >
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
-      <div className="preloader-item h-full w-[10%] bg-black"></div>
+  const name = "FORHAD KHAN";
 
-      <p className="name-text flex text-[20vw] lg:text-[200px] font-anton text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 leading-none overflow-hidden">
-        <span className="inline-block translate-y-full">F</span>
-        <span className="inline-block translate-y-full">O</span>
-        <span className="inline-block translate-y-full">R</span>
-        <span className="inline-block translate-y-full">H</span>
-        <span className="inline-block translate-y-full">A</span>
-        <span className="inline-block translate-y-full">D</span>
+  return (
+    <div ref={preloaderRef} className="fixed inset-0 z-[9999] flex bg-black">
+      {Array.from({ length: 10 }).map((_, index) => (
+        <div key={index} className="preloader-item h-full w-[10%] bg-black" />
+      ))}
+
+      <p className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 overflow-hidden text-[10vw] font-anton leading-none lg:text-[200px]">
+        {name.split("").map((char, index) => (
+          <span
+            key={index}
+            className={char === " " ? "w-[0.3em]" : "overflow-hidden"}
+          >
+            {char !== " " && (
+              <span className="name-letter inline-block translate-y-full">
+                {char}
+              </span>
+            )}
+          </span>
+        ))}
       </p>
     </div>
   );
