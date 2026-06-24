39a7f5789f52c95307744b48e993700c7d74e842
xnafi <forhadkhan2011@gmail.com>
2026-06-24 17:07:34 +0600

    Fix page container styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Skills from "@/src/components/Skills";
 
 export default function Home() {
   return (
-    <div className="page-">
+    <div className="max-w-350 px-4 lg:px-2 py-10 lg:py-0 mx-auto relative overflow-x-hidden">
       <Banner />
       <AboutMe />
       <Skills />
