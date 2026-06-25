3c2fde57892f30da5940ff20ecd55d790f5b8be1
xnafi <forhadkhan2011@gmail.com>
2026-06-25 15:47:50 +0600

    Refactor Experience to use scroll animation hook

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,53 +1,36 @@
 "use client";
 
 import { MY_EXPERIENCE } from "@/lib/data";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Link from "next/link";
 import { useRef } from "react";
 import { FiArrowUpRight } from "react-icons/fi";
 import SectionTitle from "./Re-Ui/SectionTitle";
-
-gsap.registerPlugin(ScrollTrigger);
+import { useScrollAnimation } from "@/src/hooks/useScrollAnimation";
 
 const Experiences = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useGSAP(
-    () => {
-      gsap.from(".experience-item", {
-        y: 50,
-        opacity: 0,
-        stagger: 0.2,
-        ease: "power2.out",
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "top 70%",
-          end: "bottom 50%",
-          scrub: 1,
-        },
-      });
+  useScrollAnimation({
+    scope: containerRef,
+    slideIn: {
+      target: ".experience-item",
+      y: 50,
+      opacity: 0,
+      stagger: 0.2,
+      start: "top 70%",
+      end: "bottom 50%",
+      scrub: 1,
     },
-    { scope: containerRef },
-  );
-
-  useGSAP(
-    () => {
-      gsap.to(containerRef.current, {
-        y: -150,
-        opacity: 0,
-        ease: "none",
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "bottom 50%",
-          end: "bottom 20%",
-          scrub: 1,
-        },
-      });
+    slideOut: {
+      target: ".experience-item",
+      y: -150,
+      opacity: 0,
+      stagger: 0.02,
+      start: "bottom 50%",
+      end: "bottom 20%",
+      scrub: 1,
     },
-    { scope: containerRef },
-  );
+  });
 
   return (
     <section id="my-experience" className="py-section overflow-hidden mt-20 mb-10">
