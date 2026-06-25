e021fe968f4a5d61389c88c946d7b3aa88c3731f
xnafi <forhadkhan2011@gmail.com>
2026-06-25 17:55:18 +0600

    Change page container to semantic main element

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ import Skills from "@/src/components/Skills";
 
 export default function Home() {
   return (
-    <div className="max-w-350 px-4 lg:px-2 py-10 lg:py-0 mx-auto relative overflow-x-hidden">
+    <main className="max-w-350 px-4 lg:px-2 py-10 lg:py-0 mx-auto relative overflow-x-hidden">
       <Banner />
       <AboutMe />
       <Skills />
       <Experiences />
-    </div>
+    </main>
   );
 }
