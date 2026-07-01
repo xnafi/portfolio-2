84af6972a1f82e1a205872299d121555782edfc8
xnafi <forhadkhan2011@gmail.com>
2026-07-01 18:46:27 +0600

    Remove selectedProject prop from Project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,21 +1,18 @@
 import { IProject } from "@/types";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import Image from "next/image";
 import { useRef } from "react";
 import TransitionLink from "./Re-Ui/TransitionLink";
 
 interface Props {
   index: number;
   project: IProject;
-  selectedProject: string | null;
-  onMouseEnter: (_slug: string) => void;
 }
 
 
 gsap.registerPlugin(useGSAP);
 
-const Project = ({ index, project, selectedProject, onMouseEnter }: Props) => {
+const Project = ({ index, project }: Props) => {
   const externalLinkSVGRef = useRef<SVGSVGElement>(null);
 
   const { context, contextSafe } = useGSAP(() => {}, {
@@ -25,8 +22,6 @@ const Project = ({ index, project, selectedProject, onMouseEnter }: Props) => {
 
   const handleMouseEnter = () => {
     contextSafe?.(() => {
-      onMouseEnter(project.slug);
-
       const arrowLine = externalLinkSVGRef.current?.querySelector(
         "#arrow-line",
       ) as SVGPathElement;
@@ -94,17 +89,6 @@ const Project = ({ index, project, selectedProject, onMouseEnter }: Props) => {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      {selectedProject === null && (
-        <Image
-          src={project.thumbnail}
-          alt="Project"
-          width="300"
-          height="200"
-          className="w-full object-cover mb-6 aspect-3/2 object-top"
-          key={project.slug}
-          loading="lazy"
-        />
-      )}
       <div className="flex gap-2 md:gap-5">
         <div className="font-anton text-muted-foreground">
           _{(index + 1).toString().padStart(2, "0")}.
